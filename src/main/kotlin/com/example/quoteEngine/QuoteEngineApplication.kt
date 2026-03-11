38df22d7d6141339621c6513a83b981dbdeb560a
John <john@ardenleague.com>
2026-03-11 11:11:06 +1100

    initial

---
+++ src/main/kotlin/com/example/quoteEngine/QuoteEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.example.quoteEngine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuoteEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuoteEngineApplication>(*args)
+}
